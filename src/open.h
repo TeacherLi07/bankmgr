f40a2c7684040b92b1945fca7ff95ed27da0f2b6
lzy <lzy_thomas@163.com>
2025-12-23 14:35:54 +0800

    2

---
+++ src/open.h
@@ -0,0 +1,7 @@
+#include "banklist.h"
+
+void openAccount(BankListNode* head);
+void cancelaccount(BankListNode* head);
+void printAccount(const Account& acc);
+void queryAccount(BankListNode* head, int choice, const string& targetID,
+const string& targetName, const Date& targetDate);
