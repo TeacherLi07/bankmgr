bd12db1cef4055e98b6f011646d03488ea7acf36
wj-25120294 <1966470221@qq.com>
2025-12-23 13:31:14 +0800

    1

--- src/Sort.cpp
+++ src/Sort.cpp
@@ -1,21 +1,21 @@
 #include"Sort.h"
 BankListNode* SortByName(BankListNode *head)
 {
-    if (head==nullptr)return nullptr;//如果是空，直接返回
+    if (head->next==nullptr)return nullptr;//如果是空，直接返回
     set<Account,NameCmp>ByName;
-    for (BankListNode *p =head;p!=nullptr;p = p->next)
+    for (BankListNode *p =head->next;p!=nullptr;p = p->next)
     {
         ByName.insert(p->account);//把原链表的account通过set排序
     }
-    BankListNode *newHead= nullptr;
+    BankListNode *dataHead= nullptr;
     BankListNode *tail =nullptr;
     for (const auto &it :ByName)
     {
         BankListNode *node = new BankListNode;
         node->account = it;
         node->next =nullptr;
-        if (newHead == nullptr){
-            newHead=node;
+        if (dataHead == nullptr){
+            dataHead=node;
             tail=node;
         }
         else{
@@ -23,26 +23,28 @@ BankListNode* SortByName(BankListNode *head)
             tail=node;
         }
     }
+    BankListNode *newHead =new BankListNode;
+    newHead->next=dataHead;
     return newHead;
 }
 //姓名排序实现函数
 BankListNode* SortByMoney(BankListNode *head)
 {
-    if (head==nullptr)return nullptr;//如果是空，直接返回
+    if (head->next==nullptr)return nullptr;//如果是空，直接返回
     set<Account,MoneyCmp>ByMoney;
-    for (BankListNode *p =head;p!=nullptr;p = p->next)
+    for (BankListNode *p =head->next;p!=nullptr;p = p->next)
     {
         ByMoney.insert(p->account);//把原链表的account通过set排序
     }
-    BankListNode *newHead= nullptr;
+    BankListNode *dataHead= nullptr;
     BankListNode *tail =nullptr;
     for (const auto &it :ByMoney)
     {
         BankListNode *node = new BankListNode;
         node->account = it;
         node->next =nullptr;
-        if (newHead == nullptr){
-            newHead=node;
+        if (dataHead == nullptr){
+            dataHead=node;
             tail=node;
         }
         else{
@@ -50,26 +52,28 @@ BankListNode* SortByMoney(BankListNode *head)
             tail=node;
         }
     }
+    BankListNode *newHead =new BankListNode;
+    newHead->next=dataHead;
     return newHead;
 }
 //金额排序实现函数
 BankListNode* SortByAccount(BankListNode *head)
 {
-    if (head==nullptr)return nullptr;//如果是空，直接返回
+    if (head->next==nullptr)return nullptr;//如果是空，直接返回
     set<Account,AccountCmp>ByAccount;
-    for (BankListNode *p =head;p!=nullptr;p = p->next)
+    for (BankListNode *p =head->next;p!=nullptr;p = p->next)
     {
         ByAccount.insert(p->account);//把原链表的account通过set排序
     }
-    BankListNode *newHead= nullptr;
+    BankListNode *dataHead= nullptr;
     BankListNode *tail =nullptr;
     for (const auto &it :ByAccount)
     {
         BankListNode *node = new BankListNode;
         node->account = it;
         node->next =nullptr;
-        if (newHead == nullptr){
-            newHead=node;
+        if (dataHead == nullptr){
+            dataHead=node;
             tail=node;
         }
         else{
@@ -77,6 +81,8 @@ BankListNode* SortByAccount(BankListNode *head)
             tail=node;
         }
     }
+    BankListNode *newHead =new BankListNode;
+    newHead->next=dataHead;
     return newHead;
 }
 //账号排序实现函数
